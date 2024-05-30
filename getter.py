57ad11f797e1ec2e51f0be0cfb6a6706a4e09eff
Rafael Santos <rafaelsantos32141@gmail.com>
2024-05-30 18:51:16 -0300

    Implementação da obtenção de skills, histórico escolar e projetos

--- getter.py
+++ getter.py
@@ -1,4 +1,5 @@
 import json
+from time import sleep
 from bs4 import BeautifulSoup, Tag
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,7 +17,7 @@ class Getter:
     def wait_until_redirect(self, url: str, timeout=10.):
         WebDriverWait(self.driver, timeout).until(EC.url_to_be(url))
 
-    def __get_element_by_path(self, driver: webdriver.Firefox | WebElement, path: str, timeout=3., by=By.XPATH):
+    def __get_element_by_path(self, driver: webdriver.Firefox | WebElement, path: str, timeout=5., by=By.XPATH):
         try:
             return WebDriverWait(driver, timeout).until(EC.visibility_of_element_located((by, path)))
         except:
@@ -34,13 +35,11 @@ class Getter:
         ul_element = self.__get_element_by_path(
             self.driver, "/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section/div[2]/div/div[1]/ul")
         if ul_element is None:
-            return None
+            return []
 
         soup = BeautifulSoup(self.driver.page_source, "html.parser")
         li_list: list[Tag] = soup.find_all(
-            'li', id=lambda x: x and 'profilePagedListComponent' in x)
-        if len(li_list) == 0:
-            return None
+            "li", id=lambda x: x and "profilePagedListComponent" in x)
 
         experiences = []
         for li in li_list:
@@ -70,6 +69,118 @@ class Getter:
                 {"title": title, "hirer": hirer, "duration": duration, "location": location, "info": info})
 
         return experiences
+
+    def __get_skills(self, id: str):
+        self.driver.get(
+            f"https://www.linkedin.com/in/{id}/details/skills/")
+        ul_element = self.__get_element_by_path(
+            self.driver, "/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section/div[2]/div[2]/div/div/div[1]/ul")
+        if ul_element is None:
+            return []
+
+        last_height = self.driver.execute_script(
+            "return document.body.scrollHeight")
+        for _ in range(10):
+            # Scroll down to bottom
+            self.driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
+
+            # Wait to load page
+            sleep(1)
+
+            # Calculate new scroll height and compare with last scroll height
+            new_height = self.driver.execute_script(
+                "return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+
+        soup = BeautifulSoup(self.driver.page_source, "html.parser")
+        li_list: list[Tag] = soup.find_all(
+            "li", id=lambda x: x and "profilePagedListComponent" in x)
+
+        skills = set()
+        for li in li_list:
+            skill = li.select_one(
+                "div > div > div:nth-of-type(2) > div:nth-of-type(1) > a > div > div > div > div > span:nth-of-type(1)")
+            if skill is None:
+                continue
+            skill = skill.text
+
+            skills.add(skill)
+
+        return sorted(list(skills))
+
+    def __get_projects(self, id: str):
+        self.driver.get(
+            f"https://www.linkedin.com/in/{id}/details/projects/")
+        ul_element = self.__get_element_by_path(
+            self.driver, "/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section/div[2]/div/div[1]/ul")
+        if ul_element is None:
+            return []
+
+        soup = BeautifulSoup(self.driver.page_source, "html.parser")
+        li_list: list[Tag] = soup.find_all(
+            "li", id=lambda x: x and "profilePagedListComponent" in x)
+
+        projects = []
+        for li in li_list:
+            title = li.select_one(
+                "div > div > div:nth-of-type(2) > div:nth-of-type(1) > div > div > div > div > div > span:nth-of-type(1)")
+            if title is None:
+                continue
+            title = title.text
+
+            duration = li.select_one(
+                "div > div > div:nth-of-type(2) > div:nth-of-type(1) > div > span:nth-of-type(1) > span:nth-of-type(1)")
+            duration = duration.text if duration is not None else None
+
+            association = li.select_one(
+                "div > div > div:nth-of-type(2) > div:nth-of-type(2) > ul > li:nth-of-type(1) > div > div > div:nth-of-type(2) > div > div > span:nth-of-type(1)")
+            association = association.text[15:]\
+                if association is not None else None
+
+            about = li.select_one(
+                "div > div > div:nth-of-type(2) > div:nth-of-type(2) > ul > li:nth-of-type(3) > div > ul > li > div > div > div > span:nth-of-type(1)")
+            about = about.text if about is not None else None
+
+            projects.append(
+                {"title": title, "duration": duration, "association": association, "about": about})
+
+        return projects
+
+    def __get_education(self, id: str):
+        self.driver.get(
+            f"https://www.linkedin.com/in/{id}/details/education/")
+        ul_element = self.__get_element_by_path(
+            self.driver, "/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section/div[2]/div/div[1]/ul")
+        if ul_element is None:
+            return []
+
+        soup = BeautifulSoup(self.driver.page_source, "html.parser")
+        li_list: list[Tag] = soup.find_all(
+            "li", id=lambda x: x and "profilePagedListComponent" in x)
+
+        education = []
+        for li in li_list:
+            institution = li.select_one(
+                "div > div > div:nth-of-type(2) > div:nth-of-type(1) > a > div > div > div > div > span:nth-of-type(1)")
+            if institution is None:
+                continue
+            institution = institution.text
+
+            course = li.select_one(
+                "div > div > div:nth-of-type(2) > div:nth-of-type(1) > a > span:nth-of-type(1) > span:nth-of-type(1)")
+            course = course.text if course is not None else None
+
+            duration = li.select_one(
+                "div > div > div:nth-of-type(2) > div:nth-of-type(1) > a > span:nth-of-type(2) > span:nth-of-type(1)")
+            duration = duration.text if duration is not None else None
+
+            education.append(
+                {"institution": institution, "course": course, "duration": duration})
+
+        return education
 
     def get_users_info(self, user_urls: list[str], output_file: str | None = None):
         print("Extracting users info...")
@@ -134,19 +245,25 @@ class Getter:
             # "https://www.linkedin.com/in/{id}/details/experience/"
             experiences = self.__get_experiences(id)
             # "https://www.linkedin.com/in/{id}/details/education/"
+            education = self.__get_education(id)
             # "https://www.linkedin.com/in/{id}/details/projects/"
+            projects = self.__get_projects(id)
             # "https://www.linkedin.com/in/{id}/details/skills/"
+            skills = self.__get_skills(id)
 
             users[id] = {
                 "name": name,
                 "title": title,
                 "location": location,
                 "about": about,
-                "experiences": experiences
+                "experiences": experiences,
+                "skills": skills,
+                "projects": projects,
+                "education": education,
             }
 
         if output_file is not None:
-            f = open('results.json', 'w')
+            f = open("results.json", "w")
             json.dump(users, f)
             print(f"Exported results to {output_file}")
 
